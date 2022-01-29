af9a8b9301516f68dcd2fb278c014d1646c5dd8e
Dante Barton <dantebartona.j@gmail.com>
2022-01-28 19:19:25 -0500

    UI layout #1

---
+++ lib/main.dart
@@ -0,0 +1,77 @@
+import 'dart:io';
+
+import 'package:flutter/material.dart';
+import 'package:window_size/window_size.dart';
+
+void main() {
+  WidgetsFlutterBinding.ensureInitialized();
+  if (Platform.isWindows) {
+    setWindowMaxSize(const Size(1024, 768));
+    setWindowMinSize(const Size(512, 384));
+  }
+  runApp(const Calculator());
+}
+
+class Calculator extends StatelessWidget {
+  const Calculator({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Simple Calculator',
+      theme: ThemeData(primaryColor: Colors.blue),
+      home: SimpleCalculator(),
+    );
+  }
+}
+
+class SimpleCalculator extends StatefulWidget {
+  SimpleCalculator({Key? key}) : super(key: key);
+
+  @override
+  _SimpleCalculatorState createState() => _SimpleCalculatorState();
+}
+
+class _SimpleCalculatorState extends State<SimpleCalculator> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text('Simple Calculator')),
+      body: Column(
+        children: <Widget>[
+          Container(
+            alignment: Alignment.centerRight,
+            padding: const EdgeInsets.fromLTRB(10, 20, 10, 0),
+            child: const Text(
+              "0",
+              style: TextStyle(fontSize: 38.0),
+            ),
+          ),
+          Container(
+            alignment: Alignment.centerRight,
+            padding: const EdgeInsets.fromLTRB(10, 30, 10, 0),
+            child: const Text(
+              "0",
+              style: TextStyle(fontSize: 48.0),
+            ),
+          ),
+          Expanded(
+            child: Divider(),
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              Container(
+                width: MediaQuery.of(context).size.width * .75,
+                child: Table(
+                  children: [TableRow(children: [])],
+                ),
+              )
+            ],
+          ),
+        ],
+      ),
+    );
+  }
+}
---
+++ widgets/calc_button.dart
@@ -0,0 +1,40 @@
+import 'package:flutter/material.dart';
+
+class CalculatorButton extends StatefulWidget {
+  CalculatorButton({
+    Key? key,
+    required this.buttonText,
+    required this.buttonHeight,
+    required this.buttonColor,
+  }) : super(key: key);
+
+  final String buttonText;
+  final double buttonHeight;
+  final Color buttonColor;
+  @override
+  _CalculatorButtonState createState() => _CalculatorButtonState();
+}
+
+class _CalculatorButtonState extends State<CalculatorButton> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: MediaQuery.of(context).size.height * 0.1,
+      color: Colors.redAccent,
+      child: FlatButton(
+          shape: RoundedRectangleBorder(
+              borderRadius: BorderRadius.circular(0.0),
+              side: BorderSide(
+                  color: Colors.white, width: 1, style: BorderStyle.solid)),
+          padding: EdgeInsets.all(16.0),
+          onPressed: null,
+          child: Text(
+            "C",
+            style: TextStyle(
+                fontSize: 30.0,
+                fontWeight: FontWeight.normal,
+                color: Colors.white),
+          )),
+    );
+  }
+}
